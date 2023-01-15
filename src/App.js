cdb1f30dcf66d3c27ccccf8ff521a657c35e7034
the-intern <ostclasses@gmail.com>
2023-01-15 13:24:30 -0600

    add link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to betabeta. Don't go back</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://gammapage.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>Every iota </p>
+        <a
+          className="App-link"
+          href="https://iotapage.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
