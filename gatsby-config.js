abeb565f3dc3052d50c98554fbb01965dcded906
the-intern <ostclasses@gmail.com>
2023-01-16 11:14:20 -0600

    basic changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Beta`,
+    description: `Nothing really.`,
+    author: `@_`,
+    siteUrl: `https://betapage.vercel.app/`,
   },
   plugins: [
     `gatsby-plugin-image`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,7 @@ const Layout = ({ children }) => {
             fontSize: `var(--font-sm)`,
           }}
         >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          {new Date().getFullYear()} &middot; This is the end
         </footer>
       </div>
     </>
